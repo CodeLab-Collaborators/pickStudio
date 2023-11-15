a225ce29bb1882be6575853e58869a64b285c1c4
Isaac Etor <114070166+Ajinung@users.noreply.github.com>
2023-11-15 17:19:31 +0100

    working on the auth pop up

--- pick_fe/src/components/index.ts
+++ pick_fe/src/components/index.ts
@@ -1,3 +1,4 @@
 export * from "./layouts";
 export * from "./props";
 export * from "./static";
+export * from "./forms";
---
+++ pick_fe/src/components/props/GlobalButton.tsx
@@ -0,0 +1,27 @@
+import { GlobalButtonProps } from "../../types";
+
+const GlobalButton: React.FC<GlobalButtonProps> = ({
+  children,
+  className,
+  style,
+  ...props
+}) => {
+  // default Tailwind CSS classes for the button
+  const defaultClasses =
+    " w-full text-white rounded-md py-3 text-base font-medium ease-in transition-all hover:bg-[#00000011]";
+
+  // Combine the default classes with the provided className
+  const buttonClasses = className
+    ? `${defaultClasses} ${className}`
+    : defaultClasses;
+
+  return (
+    <div>
+      <button style={style} type="button" className={buttonClasses} {...props}>
+        {children}
+      </button>
+    </div>
+  );
+};
+
+export default GlobalButton;
