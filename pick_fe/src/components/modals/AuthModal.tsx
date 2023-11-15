719f376d1dffc795b9779d9f283885c5c7526ba2
Isaac Etor <114070166+Ajinung@users.noreply.github.com>
2023-11-15 16:10:05 +0100

    working on the auth pop up

--- pick_fe/src/components/modals/AuthModal.tsx
+++ pick_fe/src/components/modals/AuthModal.tsx
@@ -1,7 +1,30 @@
-const AuthModal = () => {
+import { MdClose } from "react-icons/md";
+
+const AuthModal = ({ onClose }: { onClose: () => void }) => {
   return (
-    <div className="fixed inset-0 z-50 bg-[#0000005f] grid place-items-center">
-      <div className="w-1/2 h-1/2 bg-white"></div>
+    <div>
+      <div
+        onClick={onClose}
+        className="fixed fadeIn inset-0 bg-[#0000009c] opacity-50"
+      ></div>
+      <dialog
+        open
+        id="dialog"
+        className="fixed inset-1/4 bg-white p-6 rounded-lg z-10 w-1/3 h-[80%]"
+      >
+        {/* title */}
+        <div className="flex justify-between items-center border-b  pb-4">
+          <button onClick={onClose}>
+            <MdClose />
+          </button>
+          <p className=" text-lg ">Login or Signup</p>
+        </div>
+        <form method="dialog" className="">
+          <div className="mt-4">
+            <h2 className="text-xl font-medium">Welcome to Pickastudio</h2>
+          </div>
+        </form>
+      </dialog>
     </div>
   );
 };
--- pick_fe/src/components/static/AuthDropDown.tsx
+++ pick_fe/src/components/static/AuthDropDown.tsx
@@ -12,23 +12,21 @@ const AuthDropDown = () => {
     <div className="w-[250px] relative bg-white text-[15px] flex flex-col gap-2 rounded-xl shadow-lg">
       <p
         onClick={toggle}
-        className=" hover:bg-[#f7f7f7] text-black ease-in transition-all cursor-pointer px-6 py-2 mt-6"
+        className="hover:bg-[#f7f7f7] text-black ease-in transition-all cursor-pointer px-6 py-2 mt-6"
       >
         Sign up
       </p>
       <p
         onClick={toggle}
-        className=" hover:bg-[#f7f7f7] ease-in transition-all cursor-pointer px-6 py-2 "
+        className="hover:bg-[#f7f7f7] ease-in transition-all cursor-pointer px-6 py-2 "
       >
         Log in
       </p>
 
       <hr />
-      <p className=" text-xs tracking-wider px-6 my-5">PICKASTUDIO</p>
+      <p className="text-xs tracking-wider px-6 my-5">PICKASTUDIO</p>
 
-      <div className="absolute right-20 top-20 z-50">
-        {showAuth ? <AuthModal /> : null}
-      </div>
+      {showAuth && <AuthModal onClose={toggle} />}
     </div>
   );
 };
