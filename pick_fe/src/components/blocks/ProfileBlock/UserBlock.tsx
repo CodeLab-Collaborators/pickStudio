9fb41281ecc7cdd647e3ac1ed824756595e422ac
insignamedia <insignamedia@gmail.com>
2023-11-22 22:39:03 +0100

    worked on the product gallery

--- pick_fe/src/components/blocks/ProfileBlock/UserBlock.tsx
+++ pick_fe/src/components/blocks/ProfileBlock/UserBlock.tsx
@@ -3,7 +3,7 @@ import ProfileBox from "./ProfileBox";
 
 const UserBlock = () => {
   return (
-    <div className="w-[30%] h-fit sticky top-24 flex flex-col gap-6  items-center max-lg:my-6 max-lg:w-full max-lg:static max-lg:pb-6">
+    <div className="w-[30%] h-fit sticky top-[138px]flex flex-col gap-6  items-center max-lg:my-6 max-lg:w-full max-lg:static max-lg:pb-6">
       {/* profile avatar */}
       <ProfileBox />
 
