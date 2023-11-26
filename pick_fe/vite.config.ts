9ac18b52ed557663ab4579a15e42a5860f7a9388
PetxCode <brighterdayscodelab@gmail.com>
2023-11-26 23:01:40 +0100

    implimenting pwa offline mode

--- pick_fe/vite.config.ts
+++ pick_fe/vite.config.ts
@@ -1,7 +1,39 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      workbox: {
+        globPatterns: ["**/*"],
+      },
+      includeAssets: ["**/*"],
+      manifest: {
+        theme_color: "#910727",
+        background_color: "#850635",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        short_name: "pick a studio",
+        description: "pick a studio",
+        name: "pick a studio",
+        icons: [
+          {
+            src: "/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+
+          {
+            src: "/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
