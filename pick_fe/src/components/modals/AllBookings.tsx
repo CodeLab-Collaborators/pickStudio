820b8e64793b6d93d47e489842066d4019609f74
insignamedia <insignamedia@gmail.com>
2023-12-11 14:48:15 +0100

    working on all studio pop up

--- pick_fe/src/components/modals/AllBookings.tsx
+++ pick_fe/src/components/modals/AllBookings.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import {FC} from 'react'
 import { IoMdClose } from 'react-icons/io';
+import pic from "../../assets/3.jpg"
+import { FiHeart } from "react-icons/fi";
 
-const AllBookings = ({ onClose }: { onClose: () => void }) => {
+interface AllBookingsModalProps {
+  onClose: () => void;
+}
+
+const AllBookings:FC<AllBookingsModalProps> = ({ onClose }) => {
+
+  const Studiofeatures = ['wifi', 'space', 'parking space', 'music'];
+
+
+
     return (
         <div>
           <div className="fixed  inset-0 bg-[#000000ab]  z-50 ease-in duration-300 transition-all ">
@@ -22,15 +33,41 @@ const AllBookings = ({ onClose }: { onClose: () => void }) => {
     <div className="px-8 py-4 text-black   w-full h-full overflow-y-scroll ">
                 {/* COMPONENT    */}
 
-<div className='w-[70%] max-lg:w-[80%] m-auto max-md:w-full shadow-sm rounded-lg p-4 flex gap-8  bg-slate-100'>
+<div className='w-[70%] max-lg:w-[80%] m-auto max-md:w-full max-md:py-6  px-8 max-lg:px-0 py-10 flex gap-8  border-y'>
     {/* img */}
-    <div className='w-[300px] max-lg:w-[250px] max-md:w-[45%] max-md:h-[150px] h-[220px] '>
-        <img src="" alt="image" className='w-full h-full object-cover object-top' />
+    <div className='w-[400px] max-lg:w-[250px] max-md:w-[45%] max-md:h-[150px] h-[220px] '>
+        <img src={pic} alt="image" className='w-full h-full object-cover rounded-xl  object-top' />
     </div>
     {/* text */}
-    <div className=''>
-        <h2 className='text-[1.525rem] max-md:text-xl font-medium'>Product name</h2>
-        <h2>Product description</h2>
+    <div className='w-full'>
+     <div className='flex items-center justify-between'>
+     <h4 className='text-slate-400 font-semibold text-sm tracking-wide'>Product description</h4>
+     <button className='text-xl'><FiHeart /></button>
+     </div>
+        <h2 className='text-[1.525rem] pb-3 text-slate-700 font-semibold max-md:text-xl tracking-wide'>Product name</h2>
+        {/* seperator line */}
+        <div className='w-[50px] bg-slate-300 h-[1px]'></div>
+
+        {/* studio features */}
+        <div className='py-5'>
+        <ul className='list-disc ml-4  text-sm font-semibold text-gray-500 flex gap-x-6 items-center flex-wrap' >
+      {Studiofeatures.map((amenity, index) => (
+        <li key={index} >
+          {amenity}
+        </li>
+      ))}
+    </ul>
+        </div>
+           {/* seperator line */}
+           <div className='w-[50px] bg-slate-300 h-[1px]'></div>
+
+           {/* reviews and price */}
+           <div className='flex items-center justify-between mt-5 max-md:mt-3'>
+            <p>318 reviews</p>
+            <p> <strong className='font-semibold'>N20,000</strong>   /day</p>
+
+           </div>
+ 
 
     </div>
 
