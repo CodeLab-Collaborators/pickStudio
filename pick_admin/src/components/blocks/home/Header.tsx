4932dc7bcce7a63efb2298b05bc524c98433a1ef
insignamedia <insignamedia@gmail.com>
2024-01-22 15:00:40 +0100

    working on the admin home page layout

---
+++ pick_admin/src/components/blocks/home/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header:React.FC = () => {
+  return (
+    <div className='w-full p-5 flex items-center sticky top-0 shadow-sm bg-white '>Header</div>
+  )
+}
+
+export default Header
